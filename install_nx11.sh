32397e7997dd963213b7f56c1b3defa36a507856
Hang Zhang <hang@cs.ucr.edu>
2018-06-05 14:13:08 -0700

    Modify the angr-dev setup script

---
+++ install_nx11.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pip uninstall networkx
+git clone https://github.com/networkx/networkx.git networkx
+cd networkx
+git pull
+git checkout networkx-1.11
+python setup.py install
+cd ..
+#Should be ready to go.
+echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+echo 'Now you are using networkx v1.11 in current virtual env.'
--- setup_angr_env.sh
+++ setup_angr_env.sh
@@ -5,32 +5,33 @@
 
 mkdir $1
 cd $1
-git clone https://github.com/angr/angr-dev.git angr-dev
+git clone https://github.com/seclab-ucr/angr-dev-fiber.git angr-dev
+git -C angr-dev pull
+git -C angr-dev checkout fiber
 cd angr-dev
 # Checkout our modified angr components for FIBER.
 git clone https://github.com/seclab-ucr/angr-fiber.git angr
 git clone https://github.com/seclab-ucr/claripy-fiber.git claripy
 git clone https://github.com/seclab-ucr/cle-fiber.git cle
+git clone https://github.com/seclab-ucr/angr-management-fiber.git angr-management
+git clone https://github.com/seclab-ucr/angr-doc-fiber.git angr-doc
+git clone https://github.com/seclab-ucr/angrop-fiber.git angrop
 git -C angr checkout fiber
 git -C angr pull
 git -C claripy checkout fiber
 git -C claripy pull
 git -C cle checkout fiber
 git -C cle pull
+git -C angr-management checkout fiber
+git -C angr-management pull
+git -C angr-doc checkout fiber
+git -C angr-doc pull
+git -C angrop checkout fiber
+git -C angrop pull
 # Set up the angr-dev environment
 sudo pip install virtualenvwrapper
-./setup.sh -i -v -e $2
-# We depend on a specific version of networkx (v1.11), newest version will not work.
-workon $2
+sudo ./setup.sh -i -v -E $2
 cd ..
-pip uninstall networkx
-git clone https://github.com/networkx/networkx.git networkx
-cd networkx
-git pull
-git checkout networkx-1.11
-python setup.py install
-#Should be ready to go.
-cd ../..
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo "You are ready to go." 
 echo "workon $2 : to switch virtual environment before running FIBER scripts."
